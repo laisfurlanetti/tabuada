cf6efb4e716f5704afe88f9c67e4c546ff68dc9e
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-14 16:05:28 -0300

    atualizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado = 0;
+            
+            Console.Write("Digite um numero para gerar a tabuada: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int n1 = 0; n1 <= 10; n1++)
+            {
+                resultado = n1 * numero;
+                Console.WriteLine(numero + " X " + n1 + " = " + resultado);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
