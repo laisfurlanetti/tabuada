89b37610b1d0e9f17100dd1476b75171b4435fa7
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-25 19:15:13 -0300

    add funcao

--- Program.cs
+++ Program.cs
@@ -11,21 +11,22 @@ namespace Tabuada
         static void Main(string[] args)
         {
             int numero;
-            int resultado = 0;
             
             Console.Write("Digite um numero para gerar a tabuada: ");
             numero = Convert.ToInt32(Console.ReadLine());
-
             Console.Clear();
+            ImprimiTabuada(numero);
+            Console.Read();
+        }
+        public static void ImprimiTabuada(int numero)
+        {
+            int resultado = 0;
 
             for (int n1 = 0; n1 <= 10; n1++)
             {
                 resultado = n1 * numero;
                 Console.WriteLine(numero + " X " + n1 + " = " + resultado);
             }
-
-            Console.Read();
-
         }
     }
 }
